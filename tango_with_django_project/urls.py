1f4e94af2822195bb67b099d4df40bf80053c075
functionOverlord <Mario.L.Geuenich@gmail.com>
2019-01-15 11:39:01 +0000

    changed to django version 1.11 from 2.1

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -14,14 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+# from django.urls import path, include
+
+from django.conf.urls import url, include
 
 from rango import views as rango_views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('rango/', include('rango.urls')),
-    path('', rango_views.index, name='index'),
+    url('admin/', admin.site.urls),
+    url('rango/', include('rango.urls')),
+    url('', rango_views.index, name='index'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
